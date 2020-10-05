482e963cd90b463058fa4e6af8ecd70e447851b3
yadnyshree <70448345+yadnyshree@users.noreply.github.com>
2020-10-05 09:04:05 +0530

    Update twitter_route.js

--- routes/twitter_route.js
+++ routes/twitter_route.js
@@ -1,3 +1,5 @@
+var access_token =process.env.USER_ACCESS_TOKEN;
+var access_secret = process.env.USER_ACCESS_SECRET;
 var OAuth = require("oauth"),
   oauth = new OAuth.OAuth(
     "https://api.twitter.com/oauth/request_token",
@@ -13,57 +15,53 @@ const search_endpoint = "https://api.twitter.com/1.1/search/tweets.json?q=from",
   post_endpoint = "https://api.twitter.com/1.1/statuses/update.json",
   delete_endpoint = "https://api.twitter.com/1.1/statuses/destroy/";
 
-// Search tweets
-exports.search = async function (req, res) {
+// Search tweets top 5 tweets for any account
+exports.search = async function (req,res) {
   console.log("Searching Tweets");
   oauth.get(
     search_endpoint +
       encodeURIComponent(":" + req.query.account) +
       "&count=" +
       (req.query.count ? req.query.count : 3),
-    process.env.USER_ACCESS_TOKEN,
-    process.env.USER_ACCESS_SECRET,
+    access_token,
+    access_secret,
     function (err, data, _result) {
       if (err) {
         console.log(err);
         throw new Error(err);
-      } else {
-        console.log(data);
       }
       res.send(data);
     }
   );
 };
 
-// Post a Tweet
+// Post a Tweet on your account
 exports.post = async function (req, res) {
   console.log("Posting a Tweet");
   oauth.post(
     post_endpoint,
-    process.env.USER_ACCESS_TOKEN,
-    process.env.USER_ACCESS_SECRET,
+    access_token,
+    access_secret,
     req.body,
     "application/json",
     function (err, data, result) {
       if (err) {
         console.log(err);
         throw new Error(err);
-      } else {
-        console.log(data);
       }
       res.send(data);
     }
   );
 };
 
-//Delete a Tweet
+//Delete a Tweet from your account
 exports.delete = async function (req, res) {
   console.log("Deleting a Tweet");
   console.log(delete_endpoint + req.body.id + ".json");
   oauth.post(
     delete_endpoint + req.body.id + ".json",
-    process.env.USER_ACCESS_TOKEN,
-    process.env.USER_ACCESS_SECRET,
+    access_token,
+    access_secret,
     req.body,
     "application/json",
     function (err, data, result) {
@@ -71,7 +69,7 @@ exports.delete = async function (req, res) {
         console.log(err);
         res.sendStatus(err.statusCode);
       } else {
-        console.log(data);
+
         res.send(data);
       }
     }
